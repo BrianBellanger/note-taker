bdbaf83ebe6d9720b4b6309056876aa694fdd2b4
Brian Bellanger <brianjbellanger@gmail.com>
2021-04-21 18:02:02 -0500

    DoneExceptDelete

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-app.get('/api/notes', (req,res) => res.json(notesData));
+app.get('/api/notes', (req,res) => {
+
+    fs.readFile('db/db.json', (err,data) => {
+        res.json(JSON.parse(data));
+    })
+
+    
+});
+
+app.post('/api/notes', (req,res) => {
+
+    console.log(req.body);
+
+    fs.readFile('db/db.json', (err,data) => {
+        let notes = JSON.parse(data);
+        let len = notes.length -1;
+        console.log('notes', notes[len].id);
+        let newId=notes[len].id + 1;
+        req.body.id = newId;
+        notes.push(req.body);
+
+
+        fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+            if (err) {
+                console.log(err)
+            }
+
+            res.json(notes)
+        })
+       // res.json(JSON.parse(data));
+    })
+
+});
+
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
