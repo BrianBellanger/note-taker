8414d65f4d3c65d0310923836d8c6371a9220701
Brian Bellanger <brianjbellanger@gmail.com>
2021-04-21 16:43:23 -0500

    notesWorking-NewNext

--- Develop/server.js
+++ Develop/server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const notesData = require('./db/db.json');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
+app.get('/api/notes', (req,res) => res.json(notesData));
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
