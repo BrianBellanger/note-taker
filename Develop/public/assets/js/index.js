cebdc99a75c24d7bf8992a5bfae0319a7f520f3e
Brian Bellanger <brianjbellanger@gmail.com>
2021-04-21 15:12:23 -0500

    StartOver

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
